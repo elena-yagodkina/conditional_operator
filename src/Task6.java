1813c5119663a8a429634f200601237c670ba8de
elena_yagodkina <engineerlena@mail.ru>
2023-12-12 17:15:07 +0300

    homework_fix

--- src/Task6.java
+++ src/Task6.java
@@ -4,7 +4,7 @@ public class Task6 {
     public static void main(String[] args) {
         int totalPlaces = 102;
         int seatPlaces = 60;
-        int occupiedPlaces = 102;
+        int occupiedPlaces = 25;
 
         if (occupiedPlaces < 60) {
             System.out.println("В вагоне осталось " + (seatPlaces - occupiedPlaces) + " сидячих мест");
@@ -12,7 +12,8 @@ public class Task6 {
         if (occupiedPlaces >= 60 && occupiedPlaces < totalPlaces) {
             System.out.println("В вагоне осталось " + (totalPlaces - occupiedPlaces) + " стоячих мест");
         }
-        else  {
+        else
+            if (occupiedPlaces == 102) {
             System.out.println("В вагоне не осталось свободных мест");
         }
     }
