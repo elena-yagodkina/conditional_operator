c1875f2ea6ea57800bb87334b90ac396716bdff2
elena_yagodkina <engineerlena@mail.ru>
2023-12-12 17:27:24 +0300

    homework_fix2

--- src/Task4.java
+++ src/Task4.java
@@ -7,13 +7,14 @@ public class Task4 {
         if (age >= 2 && age <= 6) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
         }
+        else
         if (age >= 7 && age <= 17) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
         }
-        if (age >= 18 && age <= 24) {
+        else if (age >= 18 && age <= 24) {
             System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в институт");
-        } else {
-            if (age > 24)
+        }
+        else if (age > 24) {
                 System.out.println("Если возраст человека равен " + age + ", то ему пора ходить на работу");
         }
     }
--- src/Task6.java
+++ src/Task6.java
@@ -9,11 +9,10 @@ public class Task6 {
         if (occupiedPlaces < 60) {
             System.out.println("В вагоне осталось " + (seatPlaces - occupiedPlaces) + " сидячих мест");
         }
-        if (occupiedPlaces >= 60 && occupiedPlaces < totalPlaces) {
+        else if (occupiedPlaces >= 60 && occupiedPlaces < totalPlaces) {
             System.out.println("В вагоне осталось " + (totalPlaces - occupiedPlaces) + " стоячих мест");
         }
-        else
-            if (occupiedPlaces == 102) {
+        else if (occupiedPlaces == 102) {
             System.out.println("В вагоне не осталось свободных мест");
         }
     }
